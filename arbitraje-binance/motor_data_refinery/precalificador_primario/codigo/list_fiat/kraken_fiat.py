a1af66337d33abb0d4ea6b488282bc1e02c13128
Ezequiel Portales <businessportales@gmail.com>
2025-09-12 03:25:40 +0000

    chore(gitignore): desindexar 'precalificador_primario' para que quede ignorado

--- arbitraje-binance/motor_data_refinery/precalificador_primario/codigo/list_fiat/kraken_fiat.py
+++
@@ -1,7 +0,0 @@
-fiat_tokens = [
-    'AED', 'AED.HOLD', 'ARS', 'AUD', 'AUD.HOLD', 'BRL', 'CAD', 'CAD.HOLD', 'CHF', 'CHF.HOLD',
-    'CLP', 'CNY', 'COP', 'CZK', 'DKK', 'EUR', 'EUR.HOLD', 'EUR.M', 'GBP', 'GBP.HOLD',
-    'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP',
-    'PKR', 'PLN', 'RON', 'RUB', 'SAR', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'UAH', 'USD',
-    'USD.HOLD', 'USD.M', 'USDC.M', 'USDT.M', 'VND', 'ZAR'
-]
